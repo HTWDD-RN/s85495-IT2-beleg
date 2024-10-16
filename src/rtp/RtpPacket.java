6a130d0beaca257d80917aaa2d2c2a272c932c35
Keyur1876 <s85495@htw-dresden.de>
2024-10-16 09:07:59 +0200

    initial commit

---
+++ src/rtp/RtpPacket.java
@@ -0,0 +1,17 @@
+package rtp;
+
+public class RtpPacket extends RtpPacketDemo {
+
+    public RtpPacket(int PType, int Framenb, int Time, int Mar, byte[] data, int data_length) {
+        super(PType, Framenb, Time, Mar, data, data_length);
+    }
+
+    public RtpPacket(byte[] packet, int packet_size) {
+        super(packet, packet_size);
+    }
+
+    @Override
+    void setRtpHeader() {
+
+    }
+}
